d39ad8087ff560d00199318f0821fcf41fc37a24
Joseph El-Khouri <joseph.elkhouri+github@gmail.com>
2018-08-22 11:43:30 -0400

    remove demo stuff

--- tourneykiosk/tourneykiosk.py
+++ tourneykiosk/tourneykiosk.py
@@ -30,20 +30,11 @@ class TourneyKiosk():
             return """<html>
               <head></head>
               <body>
-                <form method="get" action="generate">
-                  <input type="text" value="" name="saved_val" />
-                  <button type="submit">Give it now!</button>
-                </form>
+                <p>Nothing Yet...</p>
               </body>
             </html>"""
 
-        @cherrypy.expose
-        def generate(self, saved_val=None):
-            if saved_val is not None:
-                cherrypy.session['saved_val'] = saved_val
-            if 'saved_val' not in cherrypy.session:
-                return 'no saved_val yet!'
-            return 'current saved val: ' + cherrypy.session['saved_val']
+    # ----- start server
 
     conf = {
         '/': {
