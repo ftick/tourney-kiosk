a080dfb000c88f1241f32562febe18959f729b81
Joseph El-Khouri <joseph.elkhouri+github@gmail.com>
2018-08-22 11:42:27 -0400

    make tourneykiosk class

--- tourneykiosk/tourneykiosk.py
+++ tourneykiosk/tourneykiosk.py
@@ -1,31 +1,50 @@
 import cherrypy
 import random
 import string
+from enum import Enum
 
-      
-class HelloWorld(object):
-    @cherrypy.expose
-    def index(self):
-        return """<html>
-          <head></head>
-          <body>
-            <form method="get" action="generate">
-              <input type="text" value="" name="saved_val" />
-              <button type="submit">Give it now!</button>
-            </form>
-          </body>
-        </html>"""
+# ----- enums 
 
-    @cherrypy.expose
-    def generate(self, saved_val=None):
-        if saved_val is not None:
-            cherrypy.session['saved_val'] = saved_val
-        if 'saved_val' not in cherrypy.session:
-            return 'no saved_val yet!'
-        return 'current saved val: ' + cherrypy.session['saved_val']
+class BracketType(Enum):
+    CHALLONGE = 1
+    BRAACKET = 2
+    SMASHGG = 3
 
+class PlayerBankType(Enum):
+    BRAACKET = 1
 
-def quickstart():
+class TourneyKiosk():
+    def __init__(self, tourney_vars, playerbank_vars,
+            tourney_type=BracketType.CHALLONGE, 
+            playerbank_type=PlayerBankType.BRAACKET):
+        self._tourney_type = tourney_type
+        self._tourney_vars = tourney_vars
+        self._playerbank_type = playerbank_type
+        self._playerbank_vars = playerbank_vars
+
+    # ----- server stuff
+
+    class HelloWorld(object):
+        @cherrypy.expose
+        def index(self):
+            return """<html>
+              <head></head>
+              <body>
+                <form method="get" action="generate">
+                  <input type="text" value="" name="saved_val" />
+                  <button type="submit">Give it now!</button>
+                </form>
+              </body>
+            </html>"""
+
+        @cherrypy.expose
+        def generate(self, saved_val=None):
+            if saved_val is not None:
+                cherrypy.session['saved_val'] = saved_val
+            if 'saved_val' not in cherrypy.session:
+                return 'no saved_val yet!'
+            return 'current saved val: ' + cherrypy.session['saved_val']
+
     conf = {
         '/': {
             'tools.sessions.on': True
